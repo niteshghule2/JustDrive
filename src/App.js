75e8882a75cfbcb325910ae9558a5f561dab4871
Nitesh <nitesh.ghule2012@gmail.com>
2021-09-19 09:07:26 +0530

    first time on git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Customer from './Customer/';
+import Admin from './Admin/';
+import Dealer from './Dealer/';
+import Login from './Customer/login.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path={["/", "/about", "/services", "/contact"]} component={() => <Customer />} />
+          <Route exact path="/admin" component={() => <Admin />} />
+          <Route exact path="/dealer" component={() => <Dealer />} />
+          <Route exact path="/login" component={() => <Login />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
